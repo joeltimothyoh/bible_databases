00865ac024d35f7af5de1b392154dfe01fcdac1d
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-05-18 16:08:25 +0000

    Fix generate/generate-chapters.sh invalid paths

--- generate/generate-chapters.sh
+++ generate/generate-chapters.sh
@@ -9,12 +9,13 @@ TXT_DIRECTORY=txt
 TRANSLATIONS=$( ls "$TXT_DIRECTORY" )
 
 for t in $TRANSLATIONS; do
-    ls -p "$TXT_DIRECTORY/$t" | grep -v / | while read -r BOOK; do
+    ls -p "$TXT_DIRECTORY/$t/books" | grep -v / | sort -n | while read -r BOOK; do
+        echo "Generating chapters for $TXT_DIRECTORY/$t/books/$BOOK"
         # mkdir -p "$CHAPTER_DIR"
         # E.g. 1 Genesis
         BIBLE_BOOK_TITLE_WITH_INDEX=$( echo "$BOOK" | head -n1 | sed 's@^\([0-9]\+\s\+[^-]\+\)\s\+.*@\1@')
         # E.g. 1, 10, 100
-        BIBLE_BOOK_CHAPTERS=$( cat "$TXT_DIRECTORY/$t/$BOOK" | cut -d ':' -f1 | tr -d '[' | uniq | grep -E '^[0-9]+$' )
+        BIBLE_BOOK_CHAPTERS=$( cat "$TXT_DIRECTORY/$t/books/$BOOK" | cut -d ':' -f1 | tr -d '[' | uniq | grep -E '^[0-9]+$' )
         # E.g. txt/YLT/chapters
         BIBLE_BOOK_CHAPTER_DIR="$TXT_DIRECTORY/$t/chapters"
         mkdir -p "$TXT_DIRECTORY/$t/chapters"
@@ -23,7 +24,8 @@ for t in $TRANSLATIONS; do
             # e.g. txt/YLT/chapters/1 Genesis 1.txt
             # e.g. txt/YLT/chapters/66 Revelation 22.txt
             echo "Creating chapter file: $BIBLE_BOOK_CHAPTER_DIR/$BIBLE_BOOK_TITLE_WITH_INDEX $c.txt"
-            cat "$TXT_DIRECTORY/$t/$BOOK" | grep -E "^\[$c:" > "$BIBLE_BOOK_CHAPTER_DIR/$BIBLE_BOOK_TITLE_WITH_INDEX $c.txt"
+            cat "$TXT_DIRECTORY/$t/books/$BOOK" | grep -E "^\[$c:" > "$BIBLE_BOOK_CHAPTER_DIR/$BIBLE_BOOK_TITLE $c.txt"
         done
     done
 done
+echo 'done'
