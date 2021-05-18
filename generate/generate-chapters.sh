a2326059c33efa1f9ae2b7a3dbcde59f7708588e
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-05-18 16:11:13 +0000

    Fix generate/generate-chapters.sh to generate chapters without index prefix in filename; Regenerate chapters

--- generate/generate-chapters.sh
+++ generate/generate-chapters.sh
@@ -14,6 +14,8 @@ for t in $TRANSLATIONS; do
         # mkdir -p "$CHAPTER_DIR"
         # E.g. 1 Genesis
         BIBLE_BOOK_TITLE_WITH_INDEX=$( echo "$BOOK" | head -n1 | sed 's@^\([0-9]\+\s\+[^-]\+\)\s\+.*@\1@')
+        # Strip off the index
+        BIBLE_BOOK_TITLE=$( echo "$BIBLE_BOOK_TITLE_WITH_INDEX" | sed 's@^[0-9]\+\s\+@@' )
         # E.g. 1, 10, 100
         BIBLE_BOOK_CHAPTERS=$( cat "$TXT_DIRECTORY/$t/books/$BOOK" | cut -d ':' -f1 | tr -d '[' | uniq | grep -E '^[0-9]+$' )
         # E.g. txt/YLT/chapters
@@ -23,7 +25,7 @@ for t in $TRANSLATIONS; do
         for c in $BIBLE_BOOK_CHAPTERS; do
             # e.g. txt/YLT/chapters/1 Genesis 1.txt
             # e.g. txt/YLT/chapters/66 Revelation 22.txt
-            echo "Creating chapter file: $BIBLE_BOOK_CHAPTER_DIR/$BIBLE_BOOK_TITLE_WITH_INDEX $c.txt"
+            echo "Creating chapter file: $BIBLE_BOOK_CHAPTER_DIR/$BIBLE_BOOK_TITLE $c.txt"
             cat "$TXT_DIRECTORY/$t/books/$BOOK" | grep -E "^\[$c:" > "$BIBLE_BOOK_CHAPTER_DIR/$BIBLE_BOOK_TITLE $c.txt"
         done
     done
