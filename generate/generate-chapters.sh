c5daf79c60d42cc901fdec66e60d4ece3dc8f2d3
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-05-18 16:30:10 +0000

    Remove unused commands in generate/generate-chapters.sh

--- generate/generate-chapters.sh
+++ generate/generate-chapters.sh
@@ -11,7 +11,6 @@ TRANSLATIONS=$( ls "$TXT_DIRECTORY" )
 for t in $TRANSLATIONS; do
     ls -p "$TXT_DIRECTORY/$t/books" | grep -v / | sort -n | while read -r BOOK; do
         echo "Generating chapters for $TXT_DIRECTORY/$t/books/$BOOK"
-        # mkdir -p "$CHAPTER_DIR"
         # E.g. 1 Genesis
         BIBLE_BOOK_TITLE_WITH_INDEX=$( echo "$BOOK" | head -n1 | sed 's@^\([0-9]\+\s\+[^-]\+\)\s\+.*@\1@')
         # Strip off the index
@@ -21,6 +20,7 @@ for t in $TRANSLATIONS; do
         # E.g. txt/YLT/chapters
         BIBLE_BOOK_CHAPTER_DIR="$TXT_DIRECTORY/$t/chapters"
         mkdir -p "$TXT_DIRECTORY/$t/chapters"
+
         # Debug
         for c in $BIBLE_BOOK_CHAPTERS; do
             # e.g. txt/YLT/chapters/1 Genesis 1.txt
